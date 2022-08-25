e950698f3aed53e824be6397ffdd15165e7bb56c
bharathegde00 <bharatshegdechipgi00@gmail.com>
2022-08-25 17:18:27 +0530

    new files

--- Day3train/Program.cs
+++ Day3train/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3train
+{
+    public class Program
+    {
+        public static void swapp(ref int a, ref int b)
+        {
+
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+        }
+        public static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            Console.WriteLine($"before swap a={a} b={b}");
+            swapp(ref a, ref b);
+            Console.WriteLine($"after swap a={a} b={b}");
+
+        }
+
+
+
+
+
+
+    }
+}
